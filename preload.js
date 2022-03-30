83521b9449de2eb6f9ee848a2edc2fd5c9f3136d
Aaron Ankiel <aaron.ankiel@outlook.com>
2022-03-29 22:20:49 -0400

    Checking in to pause

--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
+const config = require('./config.json')
+const fs = require('fs')
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
@@ -14,10 +16,35 @@ window.addEventListener('DOMContentLoaded', () => {
       return false
     })
     
-    document.getElementById('customs').addEventListener('click', () => {
-      const customsWin = window.open(`customs.html`, '_blank', 'popup,height=1080,width=1920,resizable=0')
-      return false
-    })
-  
 
-  })
+
+    
+
+    Object.entries(config.maps).forEach(([key,value]) => {
+      const mapGrid = document.getElementById('thumbs');
+      var imageName = (
+        Object.entries(config.extensions).forEach(([key,ext]) => {
+          let filename = `images\/${value}.${ext}`;
+          
+          try {
+            if (fs.existsSync(filename)) {
+              console.log(`Found: ${filename}`)
+              mapGrid.innerHTML = mapGrid.innerHTML + `<div class="mdl-cell mdl-cell--1-col">${value}</div>
+              <div class="mdl-cell mdl-cell--1-col"><a target="_blank" id="${value}"><img src="../${filename}" height="100px" width="100px" /></a></div>`
+            }
+
+          } catch(err) {
+            console.error(`File does not exist ${filename}`)
+            console.error(err)
+          }
+        })
+      )
+      const thisValue = value;
+      document.getElementById(thisValue).addEventListener('click', () => {
+        window.open(`${thisValue}.html`, '_blank', 'popup,height=1080,width=1920,resizable=0')
+        })
+      
+
+      })
+
+    })
