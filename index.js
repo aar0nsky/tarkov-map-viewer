b8dd96fcd5e30ac6751c9e8529ce6dfa4a1e50af
Aaron Ankiel <aaron.ankiel@outlook.com>
2022-03-29 20:54:30 -0400

    Added a bunch of stuff. Esc shortcut for closing windows, customs, styles.

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
 const path = require('path')
+var fs = require("fs");
+const { exit } = require('process');
+const { ChildProcess } = require('child_process');
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
+
+app.whenReady().then(() => {
+  var initPath = path.join(__dirname, "init.json");
+  var data;
+  try {
+    //data = JSON.parse(fs.readFileSync(initPath, 'utf8'));
+    
+  }
+  catch(e) {
+    console.log("cannot read init.json", e)
+    exit(1)
+  }
+
+  createWindow()
+  globalShortcut.register('Escape', () => {
+    if(null !== BrowserWindow.getFocusedWindow()){
+      BrowserWindow.getFocusedWindow().close()
+    }
+    console.log('Escape is pressed')
+  })
+})
+
 
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
-  
-    win.loadFile('index.html')
+  // let bounds = (data && data.bounds) ? data.bounds : {width: 800, height: 600}
+  // let props = { 
+  //   ...bounds,     
+  //   webPreferences: {
+  //   preload: path.join(__dirname, 'preload.js')
+  // }}
+  let props = {
+    width: 800, 
+    height: 600,
+    title: "Tarkov Map Viewer",
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: false
+    }
   }
+  console.log(props)
 
-app.whenReady().then(() => {
-createWindow()
-})
+  let win = new BrowserWindow(props)
+
+  // let child = new BrowserWindow({parent:win})
+  // child.loadFile('customs.html')
+  // child.show()
+  win.loadFile('src/index.html')
+}
+
+
 
 app.on('window-all-closed', () => {
+  // var data = {
+  //   bounds: win.getBounds()
+  // };
+  // try {
+  //   fs.writeFileSync(initPath, JSON.stringify(data));
+  // }
+  // catch(e) {
+  //   console.log("cannot write to init.json")
+  // }
   if (process.platform !== 'darwin') app.quit()
 })
--- preload.js
+++ preload.js
@@ -7,4 +7,17 @@ window.addEventListener('DOMContentLoaded', () => {
     for (const dependency of ['chrome', 'node', 'electron']) {
       replaceText(`${dependency}-version`, process.versions[dependency])
     }
+
+    document.getElementById('searchButton').addEventListener('click', () => {
+      let searchTerm = document.getElementById('searchInput').value
+      window.open(`https://escapefromtarkov.fandom.com/wiki/Special:Search?query=${searchTerm}&scope=internal&navigationSearch=true`, '_blank', 'height=1080,width=1920')
+      return false
+    })
+    
+    document.getElementById('customs').addEventListener('click', () => {
+      const customsWin = window.open(`customs.html`, '_blank', 'popup,height=1080,width=1920,resizable=0')
+      return false
+    })
+  
+
   })
