ba8ac991596d2e92d4ef7c8733cd1fde490b9c8d
Aaron Ankiel <aaron.ankiel@outlook.com>
2022-03-30 16:24:45 -0400

    Updated to include some changes but a ton of commented TODO.

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const { app, BrowserWindow, globalShortcut } = require('electron')
+const { app, BrowserWindow, globalShortcut, protocol } = require('electron')
 const path = require('path')
 var fs = require("fs");
-const { exit } = require('process');
-const { ChildProcess } = require('child_process');
-const config = require('./config.json')
+const { exit } = require('process')
 try {
   require('electron-reloader')(module)
 } catch (_) {}
 
-app.whenReady().then(() => {
-  var initPath = path.join(__dirname, "init.json");
-  var data;
-  try {
-    //data = JSON.parse(fs.readFileSync(initPath, 'utf8'));
-    
+var config = require("./config.json")
+
+var props = {
+  title: "Tarkov Multi Tool",
+  autoHideMenuBar: true,
+  webPreferences: {
+    nodeIntegration: true,
+    preload: path.join(__dirname, 'preload.js'),
+    contextIsolation: false,
+    nativeWindowOpen: true,
+    webSecurity: false
   }
-  catch(e) {
-    console.log("cannot read init.json", e)
-    exit(1)
-  }
+}
 
-  createWindow()
-  globalShortcut.register('Escape', () => {
-    if(null !== BrowserWindow.getFocusedWindow()){
-      BrowserWindow.getFocusedWindow().close()
-    }
-    console.log('Escape is pressed')
-  })
-})
-
-
+if(undefined === config.mainWindow) {
+  props.width = 800
+  props.height = 600
+}
+else {
+  props.width = config[mainWindow].width 
+  props.height = config[mainWindow].height
+}
 
 const createWindow = () => {
   // let bounds = (data && data.bounds) ? data.bounds : {width: 800, height: 600}
@@ -38,26 +36,65 @@ const createWindow = () => {
   //   webPreferences: {
   //   preload: path.join(__dirname, 'preload.js')
   // }}
-  let props = {
-    width: config.width ? config.width : 800, 
-    height: config.height ? config.height : 600,
-    title: "Tarkov Multi Tool",
-    autoHideMenuBar: true,
-    webPreferences: {
-      nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: false
-    }
-  }
   console.log(props)
 
   let win = new BrowserWindow(props)
+
 
   // let child = new BrowserWindow({parent:win})
   // child.loadFile('customs.html')
   // child.show()
   win.loadFile('index.html')
 }
+
+
+app.whenReady().then(() => {
+  protocol.registerFileProtocol('file', (request, callback) => {
+    const pathname = decodeURI(request.url.replace('file:///', ''));
+    callback(pathname);
+  })
+
+  var configPath = path.join(__dirname, "config.json");
+  // try {
+  //   config = //JSON.parse(fs.readFileSync(configPath, 'utf8'));
+  //   require('config.js')
+    
+  // }
+  // catch(e) {
+  //   console.log("cannot read config.json", e)
+  //   exit(1)
+  // }
+  createWindow()
+  globalShortcut.register('Escape', () => {
+    let curWindow = BrowserWindow.getFocusedWindow();
+    if(null !== curWindow){
+      //BrowserWindow.getFocusedWindow().getPosition() // TODO: print to config.json
+      // var bounds = { 'mainWindow' : curWindow.getBounds()}
+      // if(undefined !== curWindow.getParentWindow()){
+      //    bounds = { 'childWindow' : curWindow.getBounds()}
+      // }
+      // curWindow = BrowserWindow.getFocusedWindow()
+      // console.log(curWindow.getBounds())
+      
+      // console.log(JSON.stringify(config))
+      // config = {...bounds}
+      // console.log(JSON.stringify(config))
+      // fs.writeFile('config.json', JSON.stringify(config), 'utf8', (err) => {
+      //   if(err) return console.log(err);
+      // })
+      
+      curWindow.close()
+      
+    }
+    console.log('Escape is pressed') 
+  })
+})
+
+
+
+
+
+
 
 
 
--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
-const config = require('./config.json')
+var config = require('./config.json')
 const fs = require('fs')
 const path = require('path')
+const shell = require('electron').shell;
 
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
@@ -13,44 +14,54 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     document.getElementById('searchButton').addEventListener('click', () => {
-      let searchTerm = document.getElementById('searchInput').value
-      window.open(`https://escapefromtarkov.fandom.com/wiki/Special:Search?query=${searchTerm}&scope=internal&navigationSearch=true`, '_blank', 'height=1080,width=1920')
+      let searchTerm = document.getElementById('searchInput')
+      let searchWindow = window.open(`https://escapefromtarkov.fandom.com/wiki/Special:Search?query=${searchTerm.value}&scope=internal&navigationSearch=true`, '_blank', 'height=1080,width=1920')
       return false
+    })
+
+    document.getElementById('editConfig').addEventListener('click', () => {
+      shell.openPath(path.join(__dirname, 'config.json'))
     })
     
 
 
     var filenames = new Array()
     
-    
-    Object.entries(config.maps).forEach(([key,value]) => {
-      const mapGrid = document.getElementById('thumbs');
-      
-        Object.entries(config.extensions).forEach(([key,ext]) => {
-          let filename = path.join('images', `${value}.${ext}`)
-          
-          try {
-            if (fs.existsSync(filename)) {
-              console.log(`Found: ${filename}`)
-              filenames.push(`${value}.${ext}`)
-              mapGrid.innerHTML = mapGrid.innerHTML + `<div class="mdl-cell mdl-cell--2-col" id="${value}"><a target="_blank"><h6>${value}</h6>
-              <img src="${filename}" height="100px" width="100px" /></a></div>`
+    console.log(JSON.stringify(config.maps))
+    config.maps =  ["customs","factory","interchange","lighthouse","shoreline","reserve","woods"]
+    config.extensions = ["png","jpg","bmp","gif"]
+  
+    // config = {...config}
+    // fs.writeFile('config.json', JSON.stringify(config), 'utf8', (err) => {
+    //   if(err) return console.log(err);
+    // })
+      Object.entries(config.maps).forEach(([key,value]) => {
+        const mapGrid = document.getElementById('thumbs');
+        
+          Object.entries(config.extensions).forEach(([key,ext]) => {
+            let filename = path.join('images', `${value}.${ext}`)
+            
+            try {
+              if (fs.existsSync(filename)) {
+                console.log(`Found: ${filename}`)
+                filenames.push(`${value}.${ext}`)
+                mapGrid.innerHTML = mapGrid.innerHTML + `<div class="mdl-cell mdl-cell--2-col" id="${value}"><a target="_blank"><h6>${value}</h6>
+                <img src="${filename}" height="100px" width="100px" /></a></div>`
+              }
+  
+            } catch(err) {
+              console.error(`File does not exist ${filename}`)
+              console.error(err)
             }
-
-          } catch(err) {
-            console.error(`File does not exist ${filename}`)
-            console.error(err)
-          }
-        })
+          })
       })
+    
       console.log(filenames.toString())
       Object.entries(filenames).forEach(([key,value]) => {
         let id = value.split(".")
         document.getElementById(id[0]).addEventListener('click', () => {
-          window.open(path.join('images', value), '_blank', 'popup,height=1080,width=1920,resizable=0')
-          })
+          var mapWindow = window.open(path.join('images', value), '_blank', 'height=1080,width=1920,resizable=0')          
       }
       )
     })
-
-    
+})
