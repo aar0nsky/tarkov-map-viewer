0f86ba5ad84e1bd20be3f77554bb7050e01c26ec
Aaron Ankiel <aaron.ankiel@outlook.com>
2022-04-13 13:37:13 -0400

    Fixed some stuff.

--- index.js
+++ index.js
@@ -17,23 +17,22 @@ try {
   require("electron-reloader")(module)
 } catch (_) {}
 
-
-
 const initConfig = require("./config.json")
-var runningConfig = initConfig ? initConfig : 
-  {
-    "mainWindow":{"width": 800, "height": 600},
-    "maps" : [
-      "customs",
-      "factory",
-      "interchange",
-      "lighthouse",
-      "shoreline",
-      "reserve",
-      "woods",
-      "labs"
-    ]
-  }
+var runningConfig = initConfig
+  ? initConfig
+  : {
+      mainWindow: { width: 800, height: 600 },
+      maps: [
+        "customs",
+        "factory",
+        "interchange",
+        "lighthouse",
+        "shoreline",
+        "reserve",
+        "woods",
+        "labs"
+      ]
+    }
 
 var props = {
   title: "Tarkov Multi Tool",
@@ -46,24 +45,21 @@ var props = {
     webSecurity: false
   }
 }
-const loadConfig = exports.loadConfig = () => {
+const loadConfig = (exports.loadConfig = () => {
   if (undefined === runningConfig.mainWindow) {
-    runningConfig.mainWindow.width, props.width = 800
-    runningConfig.mainWindow.height, props.height = 600
-    runningConfig.mainWindow.x, props.x = 960
-    runningConfig.mainWindow.y, props.y = 540
+    runningConfig.mainWindow.width = 800
+    runningConfig.mainWindow.height = 600
+    runningConfig.mainWindow.x = 960
+    runningConfig.mainWindow.y = 540
+    
     fs.writeFile("config.json", JSON.stringify(runningConfig), "utf8", err => {
       if (err) return console.log(err)
     })
-  } else {
-    props.width = runningConfig.mainWindow.width
-    props.height = runningConfig.mainWindow.height
-    props.x = runningConfig.mainWindow.x
-    props.y = runningConfig.mainWindow.y
   }
-}
+    props = {...props, ...runningConfig.mainWindow}
+})
 
-const createWindow = exports.createWindow = () => {
+const createWindow = (exports.createWindow = () => {
   loadConfig()
 
   let mainWindow = new BrowserWindow(props)
@@ -78,30 +74,30 @@ const createWindow = exports.createWindow = () => {
 
     // Returns the screen where your window is located
     let winBounds = mainWindow.getBounds()
-    let windowScreen = screen.getDisplayNearestPoint({x: winBounds.x, y: winBounds.y})
+    let windowScreen = screen.getDisplayNearestPoint({
+      x: winBounds.x,
+      y: winBounds.y
+    })
 
     if (windowScreen.id !== externalDisplay.id) {
       return {
-        action: 'allow'
+        action: "allow"
       }
-    }
-      else {
-        return {
-          action: 'allow',
-          overrideBrowserWindowOptions: {
-            x: externalDisplay.bounds.x,
-            y: externalDisplay.bounds.y
-          }
-          
+    } else {
+      return {
+        action: "allow",
+        overrideBrowserWindowOptions: {
+          x: externalDisplay.bounds.x,
+          y: externalDisplay.bounds.y
         }
       }
+    }
   })
 
   mainWindow.on("close", () => {
     let configPath = path.join(__dirname, "config.json")
     try {
       runningConfig.mainWindow = mainWindow.getBounds()
-      console.log(runningConfig)
       fs.writeFileSync(configPath, JSON.stringify(runningConfig, null, 4))
     } catch (e) {
       console.log("cannot write to config.json")
@@ -109,63 +105,32 @@ const createWindow = exports.createWindow = () => {
     }
   })
 
-
+  // grab default menu and add my custom menu item/shortcut
   const menu = defaultMenu(app, shell)
-  // menu.splice(4, 0, {
-  //   label: "Custom",
-  //   submenu: [
-  //     {
-  //       label: "Close",
-  //       accelerator: "Esc",
-  //       click: () => {
-  //         let curWindow = BrowserWindow.getFocusedWindow()
-  //         if (null !== curWindow) {
-  //           curWindow.close()
-  //         }
-  //       }
-  //     },
-  //     {
-  //       label: "Map",
-  //       accelerator: "m",
-  //       click: () => {
-  //         return
-  //       }
-  //     }
-  //   ]
-  // })
 
-  menu.splice(5,0,new MenuItem({
-    label: 'Shortcuts',
-    submenu: [{
-      role: 'Close',
-      accelerator: 'Esc',
-      click: () => {
-        let curWindow = BrowserWindow.getFocusedWindow();
-        curWindow.screen
-        if(null !== curWindow){
-          curWindow.close()
+  menu.splice(
+    4,
+    0,
+    new MenuItem({
+      label: "Shortcuts",
+      submenu: [
+        {
+          role: "Close",
+          accelerator: "Esc",
+          click: () => {
+            let curWindow = BrowserWindow.getFocusedWindow()
+            curWindow.screen
+            if (null !== curWindow) {
+              curWindow.close()
+            }
+          }
         }
-    }
-  }]
-  }))
+      ]
+    })
+  )
 
-  // menu.append(new MenuItem({
-  //   label: 'Custom',
-  //   submenu: [{
-  //     role: 'Close',
-  //     accelerator: 'Esc',
-  //     click: () => {
-  //       let curWindow = BrowserWindow.getFocusedWindow();
-  //       if(null !== curWindow){
-  //         curWindow.close()
-  //       }
-  //   }
-  // }]
-  // }))
-
-  // Menu.setApplicationMenu(menu)
   Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
-}
+})
 
 app.whenReady().then(() => {
   protocol.registerFileProtocol("file", (request, callback) => {
@@ -173,16 +138,6 @@ app.whenReady().then(() => {
     callback(pathname)
   })
 
-
-  // try {
-  //   config = //JSON.parse(fs.readFileSync(configPath, 'utf8'));
-  //   require('config.js')
-
-  // }
-  // catch(e) {
-  //   console.log("cannot read config.json", e)
-  //   exit(1)
-  // }
   createWindow()
 })
 app.on("window-all-closed", () => {
